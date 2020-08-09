3679e94a5d38d944dfeaa7169ac0a6a3503e81d0
Bogdan <bogdan7morozyuk@gmail.com>
2020-08-09 12:05:05 +0300

    chore: configure firebase

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue';
 import App from '@/App.vue';
 import vuetify from '@/plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import config from '@/config';
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false;
+
+firebase.initializeApp(config.firebase);
+
+// eslint-disable-next-line import/prefer-default-export
+export const firestore = firebase.firestore();
 
 new Vue({
   vuetify,
